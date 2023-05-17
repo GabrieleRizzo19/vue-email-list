ca5873fd36ec7ebdbb0653aba0d8e52d07fdc73d
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-05-17 15:13:37 +0200

    print email only when they are 10

--- js/main.js
+++ js/main.js
@@ -7,19 +7,15 @@ createApp({
         }
     },
     mounted(){
-        console.log("Mounted iniziato");
 
+        // For cicle to push 10 email in emailList from an API request
         for(let i=0; i<10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response => {
-                console.log(response.data.response);
                 const email  = response.data.response;
                 this.emailList.push(email);
             }))
         }
 
-        
-
-        console.log("Mounted terminato");
     }
 }).mount("#app");
